e99545a1ec2b15ed3761b2ce6ef2b25b9580696e
Bruno de Jesus Silva <bruno.jesuspessoal@gmail.com>
2025-09-01 21:56:06 -0300

    Nível Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,38 +1,71 @@
 #include <stdio.h>
 
-int main()
-{
-    int torre;
-    int bispo;
-    int rainha;
-    int cavalo = 1;
-
-    printf("\n");
-    for (torre = 0; torre < 5; torre++)
-    {
-        printf("A Torre moveu-se para direita!\n");
+// Torre: movimento horizontal (direita)
+void moverTorre(int movimento) {
+    if (movimento > 0) {
+        printf("A torre Moveu-se para a direita!\n");
+        moverTorre(movimento - 1);
     }
-    printf("\n");
+}
 
-    for (bispo = 0; bispo < 5; bispo++)
-    {
-        printf("O Bispo Moveu-se para cima e direita!\n");
-    }
-    printf("\n");
-
-    for (rainha = 0; rainha < 8; rainha++)
-    {
-        printf("A Rainha moveu-se para esquerda!\n");
-    }
-
-    while (cavalo--)
-    {
-        for (int i = 0; i < 2; i++)
-        {
-            printf("A cavalo moveu-se para baixo!\n");
+// Bispo: movimento diagonal (cima + direita) com recursão e loops aninhados
+void moverBispo(int vertical, int horizontal) {
+    if (vertical > 0) {
+        for (int i = 0; i < horizontal; i++) {
+   
+            printf("O Bispo Moveu-se para a direita!\n");
         }
-        printf("A cavalo moveu-se para esquerda!\n");
+        printf("O Bispo Moveu-se para cima!\n");
+        moverBispo(vertical - 1, horizontal);
     }
+}
+
+// Rainha: movimento vertical (esquerda)
+void moverRainha(int movimento) {
+    if (movimento > 0) {
+        printf("A rainha Moveu-se para a esquerda!\n");
+        moverRainha(movimento - 1);
+    }
+}
+
+// Cavalo: movimento em "L" (duas casas para cima e uma para a direita)
+void moverCavalo(int movimentos) {
+    for (int i = 0; i < movimentos; i++) {
+        for (int j = 0; j < 1; j++) {
+            if (j < 2) {
+                printf("O cavalo Moveu-se para cima!\n");
+                continue;
+            }
+            if (j == 2) {
+                printf("O cavalo Moveu-se para direita!\n");
+                break;
+            }
+        }
+    }
+}
+
+int main() {
+    int casasBispoVertical = 5;
+    int casasBispoHorizontal = 1;
+    int casasRainha = 8;
+    int casasTorre = 5;
+    int movimentosCavalo = 3;
+
+    printf("----------MOVIMENTO DO BISPO----------\n");
+    moverBispo(casasBispoVertical, casasBispoHorizontal);
+    printf("--------------------------------------\n");
+
+    printf("----------MOVIMENTO DA RAINHA---------\n");
+    moverRainha(casasRainha);
+    printf("--------------------------------------\n");
+
+    printf("----------MOVIMENTO DA TORRE----------\n");
+    moverTorre(casasTorre);
+    printf("--------------------------------------\n");
+
+    printf("----------MOVIMENTO DO CAVALO---------\n");
+    moverCavalo(movimentosCavalo);
+    printf("--------------------------------------\n");
 
     return 0;
 }
